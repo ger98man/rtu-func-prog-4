8a402cae7c310f2ecc769cc2328df3f10f0e8526
germans-mjagkihs <47354774+ger98man@users.noreply.github.com>
2020-01-23 12:53:36 +0200

    Add files via upload

---
+++ core.clj
@@ -0,0 +1,17 @@
+(ns clofp-1.core)
+(defn clojurefp [vars]
+
+    (float 
+    (/  
+      ( reduce + 
+      	     ( map (fn[x] (val x))
+            	     ( filter (fn[k] (.endsWith (key k) "a")) vars )
+             )
+      )
+      
+      ( count
+        ( filter (fn[k] (.endsWith (key k) "a")) vars)
+      )
+    ) 
+  )
+)
